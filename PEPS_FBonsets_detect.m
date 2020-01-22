0459bd8944f7caa22c8d53d935301ec6e2e3a57d
Deirdre Bolger <deirdre.bolger76@gmail.com>
2020-01-22 16:45:45 +0100

    Updated and corrected version

--- PEPS_FBonsets_detect.m
+++ PEPS_FBonsets_detect.m
@@ -7,7 +7,7 @@
 % Loads in the *.bdf file, ensuring that 74 channels are included so that the 
 % ERGO1 and ERGO2 data are loaded.
 
-Dirbase = fullfile(filesep,'Volumes','KINGSTON','PEPS_Sujets','s01',filesep);
+Dirbase = fullfile(filesep,'Volumes','KINGSTON','PEPS_Sujets','s02',filesep);
 
 allfiles= dir(Dirbase);
 fileIndex = find(~[allfiles.isdir]);
@@ -47,12 +47,11 @@ for scnt = 1:length(filenom)
     EEG = pop_saveset( EEG, 'filename',char(fnom),'filepath',Dirbase);  % Saves a copy of the current resampled dataset to the current directory
     eeglab redraw
   
-
 end
 
 
 %% **************************EXTRACT PHOTODIODE ONSETS****************************
-fb_sessions = {'List1a-Film1' 'List1a-Film3' 'List1a-Film4'};  
+fb_sessions = filenom;  
 thresh_val = 0;
 fs = EEG.srate;
 [b,a] = butter(2,8./(fs/2));
@@ -62,10 +61,12 @@ onsets_ergo1 = cell(1,size(ergsig1,2));
 onsets_ergo2 = cell(1,size(ergsig1,2));
 offsets_ergo1 = cell(1,size(ergsig1,2));
 offsets_ergo2 = cell(1,size(ergsig1,2));
+Times_bloc = cell(length(fb_sessions),1);
 
 for ergcnt = 1:size(ergsig1,2)
     ergcnt
-    time = ALLEEG(ergcnt).times;
+    Times_bloc{ergcnt,1} = ALLEEG(ergcnt).times;
+    time = Times_bloc{ergcnt,1};
     
     ergsig1_curr = ergsig1{1,ergcnt};
     ergsig2_curr = ergsig2{1,ergcnt};
@@ -177,7 +178,7 @@ for ergcnt = 1:size(ergsig1,2)
     plot(time,onsets_ergo1{1,ergcnt},'or','MarkerFaceColor','r');
     hold on
     plot(time,offsets_ergo1{1,ergcnt},'og','MarkerFaceColor','g');
-    title('Original photodiode signal (detrended+filtered) with onsets (red) and offsets (green)');
+    title('Original photodiode signal (detrended+filtered) with onsets (red) and offsets (green) (ERG1)');
     
     subplot(2,2,3)
     plot(time,ergsig2_hwrst);
@@ -193,62 +194,133 @@ for ergcnt = 1:size(ergsig1,2)
     plot(time,onsets_ergo2{1,ergcnt},'or','MarkerFaceColor','r');
     hold on
     plot(time,offsets_ergo2{1,ergcnt},'og','MarkerFaceColor','g');
-    title('Original photodiode signal (detrended+filtered) with onsets (red) and offsets (green)');
+    title('Original photodiode signal (detrended+filtered) with onsets (red) and offsets (green) (ERG2)');
 
 end
 
 %% CATEGORIZE THE FEEDBACK ONSET TIMES BASED ON DURATIONS 
+triginfo_file = fullfile(filesep,'Users','bolger','Documents','work','Projects','Project-PEPs','Brain_IHM_Lists.xlsx');  %Path to excel file with trigger info. 
 
 onsets_all = cell(1,size(ergsig1,2));
 fbtypes_all = cell(1,size(ergsig1,2));
 lats_all = cell(1,size(ergsig1,2));
 onset_data = cell(1,size(ergsig1,2));
-sujnum = length(ALLEEG);
+Allergo_onsets = cell(1,size(ergsig1,2));
+bloc_num = length(filenom);
 
+trignames = {'erg1-cong' 'erg2-cong' 'erg1-incong' 'erg2-incong'};
 
-fbonset_vistypes = {'vis-cong' 'vis-incong'};
-fbonset_audtypes = {'aud-cong' 'aud-incong'};
-fbonset_visdurs = [160 320]; 
-fbonset_auddurs = [200 400]; % duration of corresponding photodiode signal in ms
- % corresponds to the 3 columns of onoffsets_aud/vis cell arrays.
-
-
-for sujcnt = 1:sujnum
-
-    dur_aud = onoffsets_ergo1{1,sujcnt}(:,3);
-    Iaud = arrayfun(@(x) nearest(fbonset_auddurs,x),dur_aud);
-    fba = fbonset_audtypes(1,Iaud);
-
-    dur_vis = onoffsets_ergo2{1,sujcnt}(:,3);
-    Ivis = arrayfun(@(x) nearest(fbonset_visdurs,x),dur_vis);
-    fbv = fbonset_vistypes(1,Ivis);
+for bcnt = 1:bloc_num
     
-    fb_all = cat(2,fba,fbv);
-    [onsets_all{1,sujcnt},idx] = sort([onoffsets_ergo1{1,sujcnt}(:,1)',onoffsets_ergo2{1,sujcnt}(:,1)'],'ascend');
-    lats_all{1,sujcnt} = arrayfun(@(tin) find(time == tin),onsets_all{1,sujcnt});
-    fbtypes_all{1,sujcnt} = fb_all(1,idx);
+    % Read in information on Lists, corresponding videos and trigger-types
+    % from xlsx file. 
+    ix = strfind(filenom{1,bcnt},'_');
+    sheetnom = filenom{1,bcnt}(ix(1)+1:ix(2)-1);
+    XIn = readtable(triginfo_file,'FileType','spreadsheet', 'sheet',sheetnom, 'ReadVariableNames',1, 'ReadRowNames',1,...
+        'TreatAsEmpty','NA', 'Range','A1:F5');
+    display(XIn);   %print to screen a table showing the properties of current list. 
+    fprintf('The current film is:\t%s\n', char(XIn{bcnt,1}));   %print to screen the current film 
     
-    onset_data{1,sujcnt}.video = string(fb_sessions{1,sujcnt});
-    onset_data{1,sujcnt}.onset_times = onsets_all{1,sujcnt}';
-    onset_data{1,sujcnt}.latencies = lats_all{1,sujcnt}';
-    onset_data{1,sujcnt}.types = fbtypes_all{1,sujcnt}';
-    
-    for cntr =1:length(lats_all{1,sujcnt})
-        ALLEEG(sujcnt).event(cntr).type = string(fbtypes_all{1,sujcnt}(cntr));
-        ALLEEG(sujcnt).event(cntr).latency = lats_all{1,sujcnt}(cntr);
-        ALLEEG(sujcnt).event(cntr).urevent = cntr;
-        ALLEEG(sujcnt).urevent(cntr).type = string(fbtypes_all{1,sujcnt}(cntr));
-        ALLEEG(sujcnt).urevent(cntr).latency = lats_all{1,sujcnt}(cntr);
+    %% Detect the current triggers according to the video-type presented. 
+    alltrigs = XIn{bcnt,2:5};
+    itrig = ~isnan(XIn{bcnt,2:5});
+    trigs_curr = alltrigs(itrig);
+    trignom_curr = trignames(itrig);
+      
+    durs_erg1onset = onoffsets_ergo1{1,bcnt}(:,3);  %durations of photodiode signal for ergo1
+    durs_erg2onset = onoffsets_ergo2{1,bcnt}(:,3);  %durations of photodiode signal for ergo2
+
+    %ERGO1 (visual) to begin with...
+    if length(trigs_curr)<=2
+        trig_erg1incong = 400;
+        trig_erg2incong = 320;
+    else
+        trig_erg1incong = trigs_curr(3);
+        trig_erg2incong = trigs_curr(4);
     end
     
-    [ALLEEG, ~, CURRENTSET] = pop_newset(ALLEEG, ALLEEG(sujcnt), CURRENTSET,'setname',char(fb_sessions{1,sujcnt}),'gui','off');
-    [ALLEEG, EEG] = eeg_store(ALLEEG, ALLEEG(sujcnt), CURRENTSET);
+    erg1incong_diff = arrayfun(@(eg1) abs(eg1-trig_erg1incong), durs_erg1onset);
+    erg1cong_diff = arrayfun(@(eg1) abs(eg1-trigs_curr(1)), durs_erg1onset);
+    [erg1min, ierg1] = min([erg1cong_diff, erg1incong_diff],[],2);    % 1=congruent; 2=incongruent
+    
+
+    %ERGO2 (auditory)...
+    erg2incong_diff = arrayfun(@(eg2) abs(eg2-trig_erg2incong), durs_erg2onset);
+    erg2cong_diff = arrayfun(@(eg2) abs(eg2-trigs_curr(2)), durs_erg2onset);
+    [erg2min, ierg2] = min([erg2cong_diff, erg2incong_diff], [],2);   % 1=congruent; 2=incongruent 
+    
+    % Determine the latencies for the ERGO1 and ERGO2 onsets
+    time = Times_bloc{bcnt,1};
+    erg1onsets = onoffsets_ergo1{:,bcnt}(:,1);
+    erg1onsets_cong = erg1onsets(ierg1==1);   %ergo1 congruent onset times
+    erg1onsets_incong = erg1onsets(ierg1==2); %ergo1 incongruent onset times
+    
+    erg2onsets = onoffsets_ergo2{:,bcnt}(:,1);
+    erg2onsets_cong = erg2onsets(ierg2==1);   %ergo2 congruent onset times
+    erg2onsets_incong = erg2onsets(ierg2==2); %ergo2 incongruent onset times
+    
+    if isempty(erg1onsets_incong) || isempty(erg2onsets_incong)
+        
+        dumerg1_cong = ones(length(erg1onsets_cong),1);
+        dumerg2_cong = ones(length(erg2onsets_cong),1).*2;
+        Allergs = [erg1onsets_cong; erg2onsets_cong];
+        Alldums = [dumerg1_cong; dumerg2_cong];
+        dum_erg = [Allergs, Alldums];
+       [onsets_all{1,bcnt},idx] = sort(dum_erg(:,1),'ascend');
+       idum_all = Alldums(idx,:);  %1=vis_cong, 2=aud_cong
+    else
+        
+        dumerg1_cong = ones(length(erg1onsets_cong),1);
+        dumerg2_cong = ones(length(erg2onsets_cong),1).*2;
+        dumerg1_incong = ones(length(erg1onsets_incong),1).*3;
+        dumerg2_incong = ones(length(erg2onsets_incong),1).*4;
+        Allergs = [erg1onsets_cong; erg2onsets_cong; erg1onsets_incong; erg2onsets_incong];
+        Alldums = [dumerg1_cong; dumerg2_cong; dumerg1_incong; dumerg2_incong];
+        dum_erg = [Allergs, Alldums];
+       [onsets_all{1,bcnt},idx] = sort(dum_erg(:,1),'ascend');
+       idum_all = Alldums(idx,:);  %1=vis_cong, 2=aud_cong, 3=vis_incong, 4=aud_incong
+        
+        
+    end 
+    
+    
+    tdiff = arrayfun(@(tin) abs(tin-onsets_all{1,bcnt}),time, 'UniformOutput', false);
+    idx_t = cellfun(@(l) sum(l==0), tdiff);
+    Allergo_onsets = time(idx_t==1);
+    fbtype = cell(length(Allergo_onsets),1);
+    lats = find(idx_t);
+    
+    %Assign trigger names to the onset times 
+    for i = 1:length(Allergo_onsets)
+        
+            fbtype{i,1}= trignames{1,idum_all(i)};
+        
+    end
+    
+    fbtypes_all{1,bcnt} = fbtype;
+    lats_all{1,bcnt} = Allergo_onsets;
+    
+    onset_data{1,bcnt}.video = string(fb_sessions{1,bcnt});
+    onset_data{1,bcnt}.onset_times = onsets_all{1,bcnt}';
+    onset_data{1,bcnt}.latencies = lats_all{1,bcnt}';
+    onset_data{1,bcnt}.types = fbtypes_all{1,bcnt}';
+    
+    for cntr =1:length(lats_all{1,bcnt})
+        ALLEEG(bcnt).event(cntr).type = string(fbtypes_all{1,bcnt}(cntr));
+        ALLEEG(bcnt).event(cntr).latency = lats(cntr);
+        ALLEEG(bcnt).event(cntr).urevent = cntr;
+        ALLEEG(bcnt).urevent(cntr).type = string(fbtypes_all{1,bcnt}(cntr));
+        ALLEEG(bcnt).urevent(cntr).latency = lats(cntr);
+    end
+    
+    [ALLEEG, ~, CURRENTSET] = pop_newset(ALLEEG, ALLEEG(bcnt), CURRENTSET,'setname',char(fb_sessions{1,bcnt}),'gui','off');
+    [ALLEEG, EEG] = eeg_store(ALLEEG, ALLEEG(bcnt), CURRENTSET);
     EEG = eeg_checkset( EEG );
-    EEG = pop_saveset( EEG, 'filename',char(fb_sessions{1,sujcnt}),'filepath',Dirbase);
+    EEG = pop_saveset( EEG, 'filename',char(fb_sessions{1,bcnt}),'filepath',Dirbase);
     eeglab redraw  
 end
 
 
-
+eeglab redraw
 
 
